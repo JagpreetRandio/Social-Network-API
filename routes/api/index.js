a5f4d9ca4e8e3922c76bbacc0e6f1a89a35a9a57
JagpreetRandio <115323708+JagpreetRandio@users.noreply.github.com>
2023-04-17 12:16:24 -0700

    fixing server.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const userRoutes = require("./userroutes");
-const thoughtRoutes = require("./thoughtroutes");
+const userRoutes = require("./userRoutes");
+const thoughtRoutes = require("./thoughtRoutes");
 
 router.use("/users", userRoutes);
 router.use("/thoughts", thoughtRoutes);
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,23 +2,23 @@ const router = require('express').Router();
 
 //all info
 const {
-    getUser,
+    getUsers,
     getSingleUser,
     createUser,
     updateUser,
     deleteUser,
     addFriend,
-    deleteFriend
+    removeFriend
 } = require('../../controllers/userController');
 
 // Get User and Posting User
-router.route('/').get(getUser).post(createUser);
+router.route('/').get(getUsers).post(createUser);
 
 // we are getting a user, putting user, and deleting the user with thier ID
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
 // we are looking at the friends with their ID, posting the friend, and deleting the friend 
-router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 // exporting all the information 
 module.exports = router;
